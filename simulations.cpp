e74ddd700c0702e525b05c7387cc5577c368c5d6
aadikumar1 <aadi.aaditya.kumar@gmail.com>
2025-05-14 00:17:08 -0400

    c++ import test

--- simulations.cpp
+++ simulations.cpp
@@ -1,1 +1,32 @@
-void orderbook
+#include <iostream>
+#include <curl/curl.h>
+#include "json.hpp"
+using json = nlohmann::json;
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
+    size_t newLength = size * nmemb;
+    s->append((char*)contents, newLength);
+    return newLength;
+}
+
+int main() {
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    std::string url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=YOUR_API_KEY";
+
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+        json j = json::parse(readBuffer);
+        std::cout << j.dump(4) << std::endl;
+    }
+
+    return 0;
+}
