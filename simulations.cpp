404fcf4518343becbf10443bdbe1e0387cb0b152
aadikumar1 <aadi.aaditya.kumar@gmail.com>
2025-05-18 23:32:28 -0400

    setup

--- simulations.cpp
+++ simulations.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <string>
 #include <curl/curl.h>
 #include "json.hpp"
-#include <string>
+
 using json = nlohmann::json;
-// 252 trading days setting GBM path 
+
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* s) {
-    size_t newLength = size * nmemb;
-    s->append((char*)contents, newLength);
-    return newLength;
+    size_t totalSize = size * nmemb;
+    s->append((char*)contents, totalSize);
+    return totalSize;
 }
 
 int main() {
@@ -15,26 +16,44 @@ int main() {
     CURLcode res;
     std::string readBuffer;
     std::string ticker = "AMT";
-    std::string url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=F8Y8DSU6J7URM9JO";
+    std::string apiKey = "F8Y8DSU6J7URM9JO";
+    std::string url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=" + apiKey;
 
+    std::cout << "Requesting URL: " << url << std::endl;
 
     curl = curl_easy_init();
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
+
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
 
-        json j = json::parse(readBuffer);
-        std::cout << j.dump(4) << std::endl;
+        if (res != CURLE_OK) {
+            std::cerr << "CURL error: " << curl_easy_strerror(res) << std::endl;
+            return 1;
+        }
+
+        try {
+            json j = json::parse(readBuffer);
+            std::cout << j.dump(4) << std::endl;
+        } catch (json::parse_error& e) {
+            std::cerr << "JSON parse error: " << e.what() << std::endl;
+            std::cerr << "Raw response: " << readBuffer << std::endl;
+        }
     }
 
     return 0;
 }
 
 // set public class for stock
-
+class orderbook {
+    public:
+    std::string stock;
+};
 
 // Set GBM parameters and muh sigma and logs
+
 //Plot trajectories check asscoaited features
